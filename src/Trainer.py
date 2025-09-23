10e71c94c47bc18b0c09b9d77f4a85a9eddf3e06
mahadevxo <mahadevsunilkumar03@gmail.com>
2025-09-23 16:48:51 +0530

    fixed

--- src/Trainer.py
+++ src/Trainer.py
@@ -104,7 +104,7 @@ class Trainer:
 
                 self.optimizer.zero_grad()
 
-                with autocast(enabled=self.use_amp):
+                with autocast(enabled=self.use_amp, device_type=self.device.type):
                     features = self.feature_vit(data)
                     cls_tokens = features[:, 0, :]
                     cls_tokens = cls_tokens.view(B, V, -1)
